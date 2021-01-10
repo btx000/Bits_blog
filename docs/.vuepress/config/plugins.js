1ea58f3e5ba9f159b57f54d76315e55372374fde
Bits <bigbits@hackx.cc>
2021-01-10 11:56:04 +0800

    add gitalk info

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -83,11 +83,11 @@ module.exports = [
     {
       choosen: 'gitalk',
       options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'xugaoyi', // GitHub仓库所有者
-        admin: ['xugaoyi'], // 对仓库有写权限的人
+        clientID: 'b332aff3ba3846c19b13',
+        clientSecret: '73a5ec46e9c849c120db5af4b06ad4442fcca0cb',
+        repo: 'Bits_blog', // GitHub 仓库
+        owner: 'btx000', // GitHub仓库所有者
+        admin: ['btx000'], // 对仓库有写权限的人
         // distractionFreeMode: true,
         pagerDirection: 'last', // 'first'正序 | 'last'倒序
         id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
